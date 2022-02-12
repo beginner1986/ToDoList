798ff9c9e63b75f75b4cdc136878dbf13e8bdc09
beginner1986 <beginner1986@gmail.com>
2022-02-12 14:52:51 +0100

    Server: Task entity class added

---
+++ server/src/main/java/todo/server/domain/Task.java
@@ -0,0 +1,23 @@
+package todo.server.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private Boolean isDone;
+}
