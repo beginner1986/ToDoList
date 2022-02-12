f83948ad0b94e07305d22a0c5970674780a59d41
beginner1986 <beginner1986@gmail.com>
2022-02-12 14:41:30 +0100

    Server: CorsConfig moved to the separate class

--- server/src/main/java/todo/server/ServerApplication.java
+++ server/src/main/java/todo/server/ServerApplication.java
@@ -2,26 +2,11 @@ package todo.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableWebMvc
 public class ServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
-            }
-        };
     }
 }
--- server/src/main/java/todo/server/controllers/TestController.java
+++ server/src/main/java/todo/server/controllers/TestController.java
@@ -3,8 +3,6 @@ package todo.server.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-
 @RestController
 public class TestController {
 
