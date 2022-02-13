923e6eae79c4b14237d32896a6da06e8385ab1d7
beginner1986 <beginner1986@gmail.com>
2022-02-13 14:57:16 +0100

    Server: initial data added to database (data.sql)

---
+++ server/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO  task (description, is_done) VALUES ('Buy something to eat', FALSE);
+INSERT INTO  task (description, is_done) VALUES ('Walk the dog', TRUE);
+INSERT INTO  task (description, is_done) VALUES ('Book the hotel', FALSE);
+INSERT INTO  task (description, is_done) VALUES ('Book the plane', FALSE);
+INSERT INTO  task (description, is_done) VALUES ('Print the itinerary', FALSE);
