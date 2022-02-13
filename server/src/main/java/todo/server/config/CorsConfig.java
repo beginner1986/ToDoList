8da1688d9fb0655fdf505833418566e00f6cf825
beginner1986 <beginner1986@gmail.com>
2022-02-13 14:29:27 +0100

    Server: DEV profile added with H2 database setup

--- server/src/main/java/todo/server/config/CorsConfig.java
+++ server/src/main/java/todo/server/config/CorsConfig.java
@@ -1,9 +1,11 @@
 package todo.server.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Profile("!prod")
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
--- server/src/main/java/todo/server/controllers/TestController.java
+++ server/src/main/java/todo/server/controllers/TestController.java
@@ -1,8 +1,10 @@
 package todo.server.controllers;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Profile("!prod")
 @RestController
 public class TestController {
 
