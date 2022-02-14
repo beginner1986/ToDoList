c23af1cdf0eb4fdc397679143d646692577eb178
beginner1986 <beginner1986@gmail.com>
2022-02-14 22:24:11 +0100

    Server: getting task by id implemented

--- server/src/main/java/todo/server/controllers/TaskController.java
+++ server/src/main/java/todo/server/controllers/TaskController.java
@@ -1,14 +1,12 @@
 package todo.server.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import todo.server.domain.Task;
 import todo.server.repository.TaskRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/task")
@@ -24,5 +22,13 @@ public class TaskController {
     @GetMapping
     public @ResponseBody List<Task> getAllTasks() {
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public @ResponseBody Task getTaskById(@PathVariable Long id) {
+        Optional<Task> task = repository.findById(id);
+        if(task.isEmpty()) return null;
+
+        return task.get();
     }
 }
