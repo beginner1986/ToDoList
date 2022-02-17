14593e11378153c7fa7a7666e38652394a5e8e61
beginner1986 <beginner1986@gmail.com>
2022-02-17 22:33:14 +0100

    Client: bootstrap spinner while data fetching added

--- client/src/components/AllTasksComponent.js
+++ client/src/components/AllTasksComponent.js
@@ -5,12 +5,24 @@ export default function AllTasksComponent() {
         url: 'http://localhost:8080/task'
     });
 
-    if(loading || !data) return "Loading...";
+    if(loading || !data) {
+        return (
+            <div style={{display: 'flex', height: 90 + 'vh'}} className="align-items-center">
+                <div className="spinner-border m-5 d-flex justify-content-center">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+            </div>
+        );
+    }
+
     if(error) return "Error!";
 
     const tasks = data.map(task => {
         return (
-            <tr key={task.id}><td>{task.description}</td><td>{task.isDone.toString()}</td></tr>
+            <tr key={task.id}>
+                <td>{task.description}</td>
+                <td>{task.isDone.toString()}</td>
+            </tr>
         );
     })
 
@@ -20,7 +32,8 @@ export default function AllTasksComponent() {
             <table>
                 <thead>
                     <tr>
-                        <td>Task description</td><td>Status</td>
+                        <td>Task description</td>
+                        <td>Status</td>
                     </tr>
                 </thead>
                 <tbody>
